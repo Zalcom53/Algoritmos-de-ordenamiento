8f462bb254221d9060c7370ec232a6a778d04e66
Daniel Antonio Quihuis <zalcom53@gmail.com>
2021-10-14 11:38:00 -0700

    .
    .

--- utileria.h
+++ utileria.h
@@ -12,10 +12,10 @@
 //---------------------------------------------------------------------------------
 //BLOQUE PARA DECLARAR FUNCIONES
 void run(); // Esta funcion no recibe parametros, se encarga de ejecutar el codfigo.
-void mergeSort(int []); // Esta funcion recibe tres arreglos de enteros.
-void insertionSort(int []); // Esta funcion recibe tres arreglos de enteros.
-void selectionSort(int []); // Esta funcion recibe tres arreglos de enteros.
-void generaArregloAleatorio(int c[]);// Esta funcion recibe un arreglo de enteros.
+float mergeSort(int [], float); // Esta funcion recibe tres arreglos de enteros.
+float insertionSort(int [], float); // Esta funcion recibe tres arreglos de enteros.
+float selectionSort(int [], float); // Esta funcion recibe tres arreglos de enteros.
+void generaArregloAleatorio(int []);// Esta funcion recibe un arreglo de enteros.
 void imprimeArreglo(int []); // Esta funcion se encarga de imprimir los valores del arreglo
 //---------------------------------------------------------------------------------
 
@@ -28,77 +28,123 @@ using namespace std;
 
 int userSelection; // Variable que nos servira para
 int arrtam = 10; // Esta variable corresponde al tamanio del arreglo
-int a[arrtam] = {}; // Arreglo que va a contener los valores a ordenar en el mejor caso
-int b[arrtam] = {}; // Arreglo que va a contener los valores a ordenar en el peor caso
+int a[arrtam] = {1,2,4,5,6,7,8,9,10}; // Arreglo que va a contener los valores a ordenar en el mejor caso(los valores estan ordenados)
+int b[arrtam] = {10,9,8,7,6,5,4,3,2,1}; // Arreglo que va a contener los valores a ordenar en el peor caso(orden descendente)
 int c[arrtam]; // Arreglo que va a contener los valores de forma aleatoria(caso promedio)
+float tiempo = 0.0; // Variable que se utiliza para medir el tiempo de ejecucion
 
 
 void run()
 
-
-  generaArregloOrdenado(a);
-  generaArregloDesordenado(b);
   generaArregloAleatorio(c);
 
   //MERGESORT
   cout << "Merge-Sort\n" << endl;
   cout << "\nMejor caso - Arreglo ordenado\n" << endl;
-  mergeSort(a);
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(a);
+  mergeSort(a, tiepo);
+  cout << "Arreglo despues de ordenar" << endl;
+  imprimeArreglo(a);
+  cout << "Tiempo de ejecucion para el mejor caso" << tiempo << endl;
   cout << "\n" << endl
+
   cout << "Peor caso - Arreglo desordenado\n"<< endl;
-  mergeSort(b);
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(b);
+  mergeSort(b, tiempo);
+  cout << "Arreglo despues de ordenar" << endl;
+  imprimeArreglo(b);
+  cout << "Tiempo de ejecucion para el peor caso" << tiempo <<endl;
   cout << "\n" << endl
+
   cout << "Caso promedio - Arreglo ordenado de forma aleatoria" << endl;
-  mergeSort(c);
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(c);
+  mergeSort(c, tiempo);
+  cout << "Arreglo despues de ordenar" << endl;
+  imprimeArreglo(c);
+  cout << "Tiempo de ejecucion para el caso promedio" << tiempo << endl;
   cout << "\n" << endl
 
   // INSERTONSORT
   cout << "Insertion-Sort\n" << endl;
   cout << "\nMejor caso - Arreglo ordenado\n" << endl;
-  insertionSort(a);
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(a);
+  insertionSort(a, tiempo);
+  cout << "Arreglo despues de ordenar" << endl;
+  imprimeArreglo(a);
+  cout << "Tiempo de ejecucion para el mejor caso" << tiempo << endl;
   cout << "\n" << endl
+
   cout << "Peor caso - Arreglo desordenado\n"<< endl;
-  insertionSort(b);
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(b);
+  insertionSort(b, tiempo);
+  cout << "Arreglo despues de ordenar" << endl;
+  imprimeArreglo(b);
+  cout << "Tiempo de ejecucion para el peor caso" << tiempo <<endl;
   cout << "\n" << endl
+
   cout << "Caso promedio - Arreglo ordenado de forma aleatoria" << endl;
-  insertionSort(c);
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(c);
+  insertionSort(c, tiempo);
+  cout << "Arreglo despues de ordenar" << endl;
+  imprimeArreglo(c);
+  cout << "Tiempo de ejecucion para el caso promedio" << tiempo << endl;
   cout << "\n" << endl
 
   //SELECTIONSORT
   cout << "Selection-Sort\n" << endl;
-  cout << "\nMejor caso - Arreglo ordenado\n" << endl;
-  selectionSort(a);
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(a);
+  selectionSort(a, tiempo);
+  cout << "Arreglo despues de ordenar" << endl;
+  imprimeArreglo(a);
+  cout << "Tiempo de ejecucion para el mejor caso" << tiempo << endl;
   cout << "\n" << endl
+
   cout << "Peor caso - Arreglo desordenado\n"<< endl;
-  selectionSort(b);
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(b);
+  selectionSort(b, tiempo);
+  cout << "Arreglo despues de ordenar" << endl;
+  imprimeArreglo(b);
+  cout << "Tiempo de ejecucion para el peor caso" << tiempo <<endl;
   cout << "\n" << endl
+
   cout << "Caso promedio - Arreglo ordenado de forma aleatoria" << endl;
-  selectionSort(c);
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(c);
+  selectionSort(c, tiempo);
+  cout << "Arreglo despues de ordenar" << endl;
+  imprimeArreglo(c);
+  cout << "Tiempo de ejecucion para el caso promedio" << tiempo << endl;
   cout << "\n" << endl
-
-
-
-
 
 
 }// Llave de cierre en la funcion run
 
 //---------------------------------------------------------------------------------
-void mergeSort(int []){
+float mergeSort(int [], tiempo){
 
+return tiempo;
 
 }// Llave de cierre en la funcion mergeSort
 //---------------------------------------------------------------------------------
 
-void insertionSort(int []){
+float insertionSort(int [], tiempo){
 
-
+return tiempo;
 }// Llave de cierre en insertionSort
 
 //---------------------------------------------------------------------------------
-void selectionSort(int []){
+float selectionSort(int [], tiempo){
 
 
+return tiempo;
 
 }// Llave de cierre en la funcion selectionSort
 //---------------------------------------------------------------------------------
