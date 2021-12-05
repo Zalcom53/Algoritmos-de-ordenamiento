a9e6b763b427d8ab9abb651e7b8fdf28cc64e35f
Antonio Quihuis <antonioquihuis@MacBook-Pro-de-Antonio.local>
2021-12-05 14:40:24 -0700

    .
    .

--- utileria.h
+++ utileria.h
@@ -38,6 +38,7 @@ float tiempo = 0.0; // Esta variable es para medir el timepo
 
 void run(){
   srand(time(0));
+  generaArregloAleatorio(c);
 
   //MERGESORT
   cout << "Mergesort\n" << endl;
@@ -59,8 +60,6 @@ void run(){
   cout << "\nTiempo de ejecucion para el peor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
   cout << "\n" << endl;
 
-  generaArregloAleatorio(c);
-
   cout << "Caso promedio - Arreglo ordenado de forma aleatoria\n" << endl;
   cout << "Arreglo antes de ordenar" << endl;
   imprimeArreglo(c);
@@ -71,66 +70,66 @@ void run(){
   cout << "\n" << endl;
 
   // INSERTONSORT
-  // cout << "--------------------------------------------------------------------\n" << endl;
-  // cout << "Insertionsort\n" << endl;
-  // cout << "Mejor caso - Arreglo ordenado\n" << endl;
-  // cout << "Arreglo antes de ordenar" << endl;
-  // imprimeArreglo(a);
-  // insertionSort(a, tiempo);
-  // cout << "\nArreglo despues de ordenar" << endl;
-  // imprimeArreglo(a);
-  // cout << "\nTiempo de ejecucion para el mejor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-  // cout << "\n" << endl;
-  //
-  // cout << "Peor caso - Arreglo desordenado\n" << endl;
-  // cout << "Arreglo antes de ordenar" << endl;
-  // imprimeArreglo(b);
-  // insertionSort(b, tiempo);
-  // cout << "\nArreglo despues de ordenar" << endl;
-  // imprimeArreglo(b);
-  // cout << "\nTiempo de ejecucion para el peor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-  // cout << "\n" << endl;
-  //
-  // cout << "Caso promedio - Arreglo ordenado de forma aleatoria\n" << endl;
-  // cout << "Arreglo antes de ordenar" << endl;
-  // imprimeArreglo(c);
-  // insertionSort(c, tiempo);
-  // cout << "\nArreglo despues de ordenar" << endl;
-  // imprimeArreglo(c);
-  // cout << "\nTiempo de ejecucion para el caso promedio " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-  // cout << "\n" << endl;
-  //
-  //
-  // //SELECTIONSORT
-  // cout << "--------------------------------------------------------------------\n" << endl;
-  // cout << "Selectionsort\n" << endl;
-  // cout << "Mejor caso - Arreglo ordenado\n" << endl;
-  // cout << "Arreglo antes de ordenar" << endl;
-  // imprimeArreglo(a);
-  // selectionSort(a, tiempo);
-  // cout << "\nArreglo despues de ordenar" << endl;
-  // imprimeArreglo(a);
-  // cout << "\nTiempo de ejecucion para el mejor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-  // cout << "\n" << endl;
-  //
-  // cout << "Peor caso - Arreglo desordenado\n" << endl;
-  // cout << "Arreglo antes de ordenar" << endl;
-  // imprimeArreglo(b);
-  // selectionSort(b, tiempo);
-  // cout << "\nArreglo despues de ordenar" << endl;
-  // imprimeArreglo(b);
-  // cout << "\nTiempo de ejecucion para el peor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-  // cout << "\n" << endl;
-  //
-  // cout << "Caso promedio - Arreglo ordenado de forma aleatoria\n" << endl;
-  // cout << "Arreglo antes de ordenar" << endl;
-  // imprimeArreglo(c);
-  // selectionSort(c, tiempo);
-  // cout << "\nArreglo despues de ordenar" << endl;
-  // imprimeArreglo(c);
-  // cout << "\nTiempo de ejecucion para el caso promedio " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-  // cout << "\n" << endl;
-  //
+  cout << "--------------------------------------------------------------------\n" << endl;
+  cout << "Insertionsort\n" << endl;
+  cout << "Mejor caso - Arreglo ordenado\n" << endl;
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(a);
+  insertionSort(a, tiempo);
+  cout << "\nArreglo despues de ordenar" << endl;
+  imprimeArreglo(a);
+  cout << "\nTiempo de ejecucion para el mejor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
+  cout << "\n" << endl;
+
+  cout << "Peor caso - Arreglo desordenado\n" << endl;
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(b);
+  insertionSort(b, tiempo);
+  cout << "\nArreglo despues de ordenar" << endl;
+  imprimeArreglo(b);
+  cout << "\nTiempo de ejecucion para el peor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
+  cout << "\n" << endl;
+
+  cout << "Caso promedio - Arreglo ordenado de forma aleatoria\n" << endl;
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(c);
+  insertionSort(c, tiempo);
+  cout << "\nArreglo despues de ordenar" << endl;
+  imprimeArreglo(c);
+  cout << "\nTiempo de ejecucion para el caso promedio " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
+  cout << "\n" << endl;
+
+
+  //SELECTIONSORT
+  cout << "--------------------------------------------------------------------\n" << endl;
+  cout << "Selectionsort\n" << endl;
+  cout << "Mejor caso - Arreglo ordenado\n" << endl;
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(a);
+  selectionSort(a, tiempo);
+  cout << "\nArreglo despues de ordenar" << endl;
+  imprimeArreglo(a);
+  cout << "\nTiempo de ejecucion para el mejor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
+  cout << "\n" << endl;
+
+  cout << "Peor caso - Arreglo desordenado\n" << endl;
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(b);
+  selectionSort(b, tiempo);
+  cout << "\nArreglo despues de ordenar" << endl;
+  imprimeArreglo(b);
+  cout << "\nTiempo de ejecucion para el peor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
+  cout << "\n" << endl;
+
+  cout << "Caso promedio - Arreglo ordenado de forma aleatoria\n" << endl;
+  cout << "Arreglo antes de ordenar" << endl;
+  imprimeArreglo(c);
+  selectionSort(c, tiempo);
+  cout << "\nArreglo despues de ordenar" << endl;
+  imprimeArreglo(c);
+  cout << "\nTiempo de ejecucion para el caso promedio " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
+  cout << "\n" << endl;
+
 
 
 }// Llave de cierre en la funcion run
@@ -185,7 +184,7 @@ for (int i = 0; i < 10 -1 ; i++) {
 void imprimeArreglo(int []){
 
   for (int i = 0; i < 10 ; i++) {
-    
+
     cout << a[i] << " ";
 
   }// Llave de cierre en el ciclo for
