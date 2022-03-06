91c7e4d2d64c34dc1aab535f830fe11ef2942764
Antonio Quihuis <antonioquihuis@MacBook-Pro-de-Antonio.local>
2022-03-05 22:56:24 -0700

    .
    .

--- utileria.h
+++ utileria.h
@@ -18,6 +18,7 @@ float selectionSort(int [], float); // Esta funcion recibe tres arreglos de ente
 void generaArregloAleatorio(int []);// Esta funcion recibe un arreglo de enteros.
 void imprimeArreglo(int []); // Esta funcion se encarga de imprimir los valores del arreglo
 int CapturaSegura(const char solicitud[]); // Esta funcion captura un numero entero de forma segura
+void imprimeMenu(); // Esta fucion no recibe parametros y se encarga de imprimir un menu
 //---------------------------------------------------------------------------------
 
 
@@ -34,106 +35,6 @@ int a[10] = {1,2,4,5,6,7,8,9,10}; // Arreglo que va a contener los valores a ord
 int b[10] = {10,9,8,7,6,5,4,3,2,1}; // Arreglo que va a contener los valores a ordenar en el peor caso(orden descendente)
 int c[10]; // Arreglo que va a contener los valores de forma aleatoria(caso promedio)
 float tiempo = 0.0; // Esta variable es para medir el timepo
-
-
-
-// void run(){
-//   srand(time(0));
-//   generaArregloAleatorio(c);
-//
-//   //MERGESORT
-//   cout << "Mergesort\n" << endl;
-//   cout << "Mejor caso - Arreglo ordenado\n" << endl;
-//   cout << "Arreglo antes de ordenar" << endl;
-//   imprimeArreglo(a);
-//   mergeSort(a, tiempo);
-//   cout << "\nArreglo despues de ordenar" << endl;
-//   imprimeArreglo(a);
-//   cout << "\nTiempo de ejecucion para el mejor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-//   cout << "\n" << endl;
-//
-//   cout << "Peor caso - Arreglo desordenado\n" << endl;
-//   cout << "Arreglo antes de ordenar" << endl;
-//   imprimeArreglo(b);
-//   mergeSort(b, tiempo);
-//   cout << "\nArreglo despues de ordenar" << endl;
-//   imprimeArreglo(b);
-//   cout << "\nTiempo de ejecucion para el peor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-//   cout << "\n" << endl;
-//
-//   cout << "Caso promedio - Arreglo ordenado de forma aleatoria\n" << endl;
-//   cout << "Arreglo antes de ordenar" << endl;
-//   imprimeArreglo(c);
-//   mergeSort(c, tiempo);
-//   cout << "\nArreglo despues de ordenar" << endl;
-//   imprimeArreglo(c);
-//   cout << "\nTiempo de ejecucion para el caso promedio " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-//   cout << "\n" << endl;
-//
-//   // INSERTONSORT
-//   cout << "--------------------------------------------------------------------\n" << endl;
-//   cout << "Insertionsort\n" << endl;
-//   cout << "Mejor caso - Arreglo ordenado\n" << endl;
-//   cout << "Arreglo antes de ordenar" << endl;
-//   imprimeArreglo(a);
-//   insertionSort(a, tiempo);
-//   cout << "\nArreglo despues de ordenar" << endl;
-//   imprimeArreglo(a);
-//   cout << "\nTiempo de ejecucion para el mejor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-//   cout << "\n" << endl;
-//
-//   cout << "Peor caso - Arreglo desordenado\n" << endl;
-//   cout << "Arreglo antes de ordenar" << endl;
-//   imprimeArreglo(b);
-//   insertionSort(b, tiempo);
-//   cout << "\nArreglo despues de ordenar" << endl;
-//   imprimeArreglo(b);
-//   cout << "\nTiempo de ejecucion para el peor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-//   cout << "\n" << endl;
-//
-//   cout << "Caso promedio - Arreglo ordenado de forma aleatoria\n" << endl;
-//   cout << "Arreglo antes de ordenar" << endl;
-//   imprimeArreglo(c);
-//   insertionSort(c, tiempo);
-//   cout << "\nArreglo despues de ordenar" << endl;
-//   imprimeArreglo(c);
-//   cout << "\nTiempo de ejecucion para el caso promedio " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-//   cout << "\n" << endl;
-//
-//
-//   //SELECTIONSORT
-//   cout << "--------------------------------------------------------------------\n" << endl;
-//   cout << "Selectionsort\n" << endl;
-//   cout << "Mejor caso - Arreglo ordenado\n" << endl;
-//   cout << "Arreglo antes de ordenar" << endl;
-//   imprimeArreglo(a);
-//   selectionSort(a, tiempo);
-//   cout << "\nArreglo despues de ordenar" << endl;
-//   imprimeArreglo(a);
-//   cout << "\nTiempo de ejecucion para el mejor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-//   cout << "\n" << endl;
-//
-//   cout << "Peor caso - Arreglo desordenado\n" << endl;
-//   cout << "Arreglo antes de ordenar" << endl;
-//   imprimeArreglo(b);
-//   selectionSort(b, tiempo);
-//   cout << "\nArreglo despues de ordenar" << endl;
-//   imprimeArreglo(b);
-//   cout << "\nTiempo de ejecucion para el peor caso " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-//   cout << "\n" << endl;
-//
-//   cout << "Caso promedio - Arreglo ordenado de forma aleatoria\n" << endl;
-//   cout << "Arreglo antes de ordenar" << endl;
-//   imprimeArreglo(c);
-//   selectionSort(c, tiempo);
-//   cout << "\nArreglo despues de ordenar" << endl;
-//   imprimeArreglo(c);
-//   cout << "\nTiempo de ejecucion para el caso promedio " << float(tiempo)/CLOCKS_PER_SEC << " segundos."<< endl;
-//   cout << "\n" << endl;
-//
-//
-//
-// }// Llave de cierre en la funcion run
 
 //---------------------------------------------------------------------------------
 float mergeSort(int [], float){
@@ -212,17 +113,14 @@ int CapturaSegura(const char solicitud[]){
              continue;
         }
 
-         // if(userSelection < OPMIN || userSelection > OPMAX){
-         //
-//         //   cout << "Error: Opcion fuera de rango\n" << endl;
-//         //   cout << "Intente con una opcion valida\n" << endl;
-//         //
-//         //   userSelection = CapturaEntero("Ingrese una opcion valida");
-//         //
-//         // }//LLave de cierre en if
-//
-//         return n;
+       return n;
      }
  }//Llave de cierre en la funcion CapturaEntero
+ //---------------------------------------------------------------------------------
+void imprimeMenu(){
+  cout << "Menu\n" << endl;
+
+
+}// Llave de cierre en la funcion imprimeMenu
 //---------------------------------------------------------------------------------
 #endif //TAREA_1_UTILERIA_H
