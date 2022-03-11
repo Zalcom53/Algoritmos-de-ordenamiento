d12703d075e341263396211a1e6fc6a34b5bdf12
Daniel Antonio Quihuis <zalcom53@gmail.com>
2022-03-11 11:53:16 -0700

    .
    .

--- utileria.h
+++ utileria.h
@@ -15,7 +15,9 @@ void run(); // Esta funcion no recibe parametros, se encarga de ejecutar el codf
 float mergeSort(int [], float); // Esta funcion recibe tres arreglos de enteros.
 float insertionSort(int [], float); // Esta funcion recibe tres arreglos de enteros.
 float selectionSort(int [], float); // Esta funcion recibe tres arreglos de enteros.
-void generaArregloAleatorio(int []);// Esta funcion recibe un arreglo de enteros.
+void generaArregloAleatorio(int []);// Esta funcion recibe un arreglo de enteros y los ordena de forma aleatoria
+void generaArregloOrdenado(); // Esta funcion recibe un arreglo de enteros y los ordena de forma odenada
+void generaArregloDesordenado(); // Esta funcion recibe un arreglo de enteros y los ordena de forma desodenada
 void imprimeArreglo(int []); // Esta funcion se encarga de imprimir los valores del arreglo
 int CapturaSegura(const char solicitud[]); // Esta funcion captura un numero entero de forma segura
 int defineSecuencia(); // Esta funcion toma los valores para la secuencia de ejecucion
@@ -33,6 +35,7 @@ using namespace std;
 //---------------------------------------------------------------------------------
 float mergeSort(int [], float){
 
+
 clock_t t; // inicar el contador
 t = clock();
 
@@ -43,10 +46,20 @@ return tiempo;
 }// Llave de cierre en la funcion mergeSort
 //---------------------------------------------------------------------------------
 
-float insertionSort(int [], float){
-
+float insertionSort(int [], int){
+int pos, aux;
 clock_t t; // inicar el contador
 t = clock();
+
+for (int i = 0; i < n; i++) {
+
+pos = i;
+aux = a[i];
+while ((pos > 0) && a[pos-1] > aux) {
+  lugar--;
+      } // LLave de cierre en el ciclo while
+    a[pos] = aux;
+   } //LLave de cierre en el ciclo for
 
 t = clock()-t; // Para detener el contador
 tiempo = t;
@@ -56,8 +69,21 @@ return tiempo;
 //---------------------------------------------------------------------------------
 float selectionSort(int [], float){
 
+int min, aux;
 clock_t t; // inicar el contador
 t = clock();
+
+for (int i = 0; i < n; i++) {
+  min = i;
+  for (int j = i+1; j < n; j++) {
+    if (a[j] < a[min]) {
+      min = j;
+    }// LLave de cierre en if  dentro de for con j
+  }// Llave de cierre para for para j
+  aux = a[i];
+  a[i] = a[min];
+  a[min] = aux;
+} // Llave de cierre para for con i
 
 t = clock()-t; // Para detener el contador
 tiempo = t;
@@ -74,9 +100,26 @@ for (int i = 0; i < 10 -1 ; i++) {
 
 }// Llave de cierre en el ciclo for
 
+cout << "Arreglo generado con exito." << endl;
 
 }// Llave de cierre en la funcion generaArregloAleatorio
 //---------------------------------------------------------------------------------
+void generaArregloOrdenado(){
+
+cout << "\nGenerando arreglo de forma ordenada, espere un momento...\n" << endl;
+
+cout << "Arreglo generado con exito" << endl;
+}// Llave de cierre en la funcion generaArregloOrdenado
+//---------------------------------------------------------------------------------
+void generaArregloDesordenado(){
+
+cout << "\nGenerando arreglo de forma desordenada, espere un momento...\n" << endl;
+
+
+cout << "Arreglo generado con exito" << endl;
+}// Llave de cierre en la funcion generaArregloDesordenado
+//---------------------------------------------------------------------------------
+
 void imprimeArreglo(int []){
 
   for (int i = 0; i < 10 ; i++) {
@@ -125,9 +168,9 @@ int defineSecuencia(int, int, int){
   tipo_arreglo = CapturaSegura("Seleccione el orden del arreglo");
 
   cout << "Seleccione el algoritmo de ordenamiento: "
-  cout << "MERGESORT [1]" << endl;
-  cout << "INSERTONSORT [2]" << endl;
-  cout << "SELECTIONSORT [3]" << endl;
+  cout << "Merge-Sort [1]" << endl;
+  cout << "Insertion-Sort [2]" << endl;
+  cout << "Selection-Sort [3]" << endl;
   tipo_algoritmo = CapturaSegura("Seleccione una algoritmo de ordenamiento");
 
   return nTAM, tipo_arreglo, tipo_algoritmo;
